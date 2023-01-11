aaa9aacfa1a5a962ae7f50758975bd3bd28155a2
João Cigana <joaovitorcigana@gmail.com>
2023-01-10 21:13:20 -0300

    Authentication server microservice created.
    Basic configuration done.

---
+++ hr-oauth/src/main/java/dev/cigana/hroauth/HrOauthApplication.java
@@ -0,0 +1,15 @@
+package dev.cigana.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class HrOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrOauthApplication.class, args);
+	}
+
+}
--- hr-user/src/main/java/dev/cigana/hruser/controllers/UserController.java
+++ hr-user/src/main/java/dev/cigana/hruser/controllers/UserController.java
@@ -4,7 +4,6 @@ package dev.cigana.hruser.controllers;
 import dev.cigana.hruser.domain.User;
 import dev.cigana.hruser.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
